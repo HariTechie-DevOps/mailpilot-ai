39941a95216cd5b1b5039d5c4b33893674d5cfda
Harisankar <harisankar2206@gmail.com>
2026-05-02 16:37:55 +0530

    Update MailpilotApplication.java

--- src/main/java/com/mailpilot/MailpilotApplication.java
+++ src/main/java/com/mailpilot/MailpilotApplication.java
@@ -3,7 +3,7 @@ package com.mailpilot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mailpilot")
 public class MailpilotApplication {
     public static void main(String[] args) {
         SpringApplication.run(MailpilotApplication.class, args);
