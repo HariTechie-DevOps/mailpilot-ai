1f29c321d251fba233f709219be96e9178a3296f
Harisankar <harisankar2206@gmail.com>
2026-05-02 11:35:06 +0530

    Add EmailLogRepository interface for EmailLog entity

---
+++ src/main/java/com/mailpilot/repository/EmailLogRepository.java
@@ -0,0 +1,7 @@
+package com.mailpilot.repository;
+
+import com.mailpilot.model.EmailLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailLogRepository extends JpaRepository<EmailLog, Long> {
+}
