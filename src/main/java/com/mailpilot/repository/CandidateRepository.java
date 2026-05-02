528d2e78459dc0eec5c46215a6affc917de40ec0
Harisankar <harisankar2206@gmail.com>
2026-05-02 15:24:30 +0530

    Add findByStatus method to CandidateRepository

--- src/main/java/com/mailpilot/repository/CandidateRepository.java
+++ src/main/java/com/mailpilot/repository/CandidateRepository.java
@@ -3,5 +3,9 @@ package com.mailpilot.repository;
 import com.mailpilot.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+
+    List<Candidate> findByStatus(String status);
 }
