6ad6eb7433dd60012d128147a6a24987e34acc70
Harisankar <harisankar2206@gmail.com>
2026-05-07 10:39:47 +0530

    Update GmailReaderService.java

--- src/main/java/com/mailpilot/service/GmailReaderService.java
+++ src/main/java/com/mailpilot/service/GmailReaderService.java
@@ -1,5 +1,23 @@
-public List<String> readEmails() {
-    // Connect Gmail API
-    // Fetch unread emails
-    // Return email bodies
+package com.mailpilot.service;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
+public class GmailReaderService {
+
+    public List<String> readEmails() {
+
+        // Connect Gmail API
+        // Fetch unread emails
+        // Return email bodies
+
+        List<String> emails = new ArrayList<>();
+
+        emails.add("Yes I confirm my interview");
+        emails.add("Can we reschedule?");
+
+        return emails;
+    }
 }
