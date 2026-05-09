11e5772fba246ebbd74bf79bba1f77a1a006f9d8
Harisankar <harisankar2206@gmail.com>
2026-05-09 11:54:09 +0530

    Update GmailReaderService.java

--- src/main/java/com/mailpilot/service/GmailReaderService.java
+++ src/main/java/com/mailpilot/service/GmailReaderService.java
@@ -14,7 +14,7 @@ import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
 import com.mailpilot.model.IncomingEmail;
 import org.springframework.stereotype.Service;
-
+import java.util.Scanner;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
